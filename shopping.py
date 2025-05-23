29cca22df2ee6c33dd399fe2adebbccdf0570f8b
dawid <dawid.petkowicz123wp.pl@wp.pl>
2025-05-23 10:55:13 +0200

    Print formatted shopping list with capitalized names

--- shopping.py
+++ shopping.py
@@ -4,3 +4,7 @@ shopping_list = {
     "lidl": ["mleko", "jajka", "sok"],
     "kaufland": ["ryż", "makaron", "oliwa"]
 }
+
+for shop, items in shopping_list.items():
+    capitalized_items = [item.capitalize() for item in items]
+    print(f"Idę do {shop.capitalize()} i kupuję tam {', '.join(capitalized_items)}.")
