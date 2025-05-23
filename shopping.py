f5fc4d877afa9028804acd0fc7efe29a6f18130b
dawid <dawid.petkowicz123wp.pl@wp.pl>
2025-05-23 10:56:58 +0200

    Display total number of products

--- shopping.py
+++ shopping.py
@@ -8,3 +8,7 @@ shopping_list = {
 for shop, items in shopping_list.items():
     capitalized_items = [item.capitalize() for item in items]
     print(f"Idę do {shop.capitalize()} i kupuję tam {', '.join(capitalized_items)}.")
+
+    total_items = sum(len(items) for items in shopping_list.values())
+print(f"\nW sumie kupuję {total_items} produktów.")
+
